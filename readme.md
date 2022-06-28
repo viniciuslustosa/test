ab5bb5037739faa14f118feade573bd5879be61d
Vinicius Lustosa <59030837+viniciuslustosa@users.noreply.github.com>
2022-06-28 16:42:49 -0300

    feat(add): initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+Execução do código usando Node.js no terminal
+
+```node test.js```
---
+++ test.js
@@ -0,0 +1,22 @@
+isEsotericOrSkeptical = (n) => {
+  if(n % 3 === 0 && n % 5 === 0) {
+    return 'É esotérico';
+  } else {
+    return 'É cético';
+  }
+}
+
+isPrimeNumber = (n) => {
+  for(i = 2; i < n; i++) {
+    if(n % i === 0) {
+      return 'Não é número primo'
+    }
+  }
+  
+  return 'É número primo'
+}
+
+const numberHere = 1
+
+console.log(isEsotericOrSkeptical(numberHere));
+console.log(isPrimeNumber(numberHere));
